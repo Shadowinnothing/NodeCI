14e20dd5f36f5dcb44b2862b88bd16a1d51bb602
Shadowinnothing <shadowinnothing@gmail.com>
2018-07-23 18:51:36 -0600

    Beginning of File Upload

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,7 @@ module.exports = {
   //mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   mongoURI: 'mongodb://testUser_dev:password12@ds053216.mlab.com:53216/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  acessKeyId: 'AKIAJVIFC6HENKRT5YTQ',
+  secretAccessKey: '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'
 };
--- index.js
+++ index.js
@@ -14,7 +14,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
 const app = express();
-const gel = 'badass';
 
 app.use(bodyParser.json());
 app.use(
@@ -28,6 +27,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
+require('./routes/uploadRoutes')(app);
 
 if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
